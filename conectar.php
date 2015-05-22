80ce6b5851d5fad47d8f09bf438b2fbf1c3f8ed2
luchovelez <shenlong87@gmail.com>
2015-05-22 00:05:17 -0500

    version corregida

--- conectar.php
+++ conectar.php
@@ -1,9 +1,9 @@
 <?php
 function conectar(){
-$user="a6784434_notes";
-$pass="Luisvelez87";
-$server="mysql5.000webhost.com";
-$db="a6784434_notes";
+$user="root";
+$pass="password";
+$server="localhost";
+$db="audinotes";
 $con=@mysql_connect($server,$user,$pass) or die ("Error de Conexion");
 mysql_select_db($db,$con);
 return $con;
